36f0d8bed07697f7966688c59f7fd816922cf921
fasinfrankie <alexandra.tyrrell@kellogg.northwestern.edu>
2022-03-13 02:38:03 +0000

    fixing layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome."
+        flash[:notice] = "Welcome!"
         redirect_to "/posts"
       else
         flash[:notice] = "Nope."
