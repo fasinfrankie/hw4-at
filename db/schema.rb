9842613a5f018f9fff9f0b2b0f370f5711edfda5
fasinfrankie <alexandra.tyrrell@kellogg.northwestern.edu>
2022-03-14 22:47:47 +0000

    gem

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_020701) do
     t.string "title"
     t.text "description"
     t.date "posted_on"
+    t.string "image"
     t.integer "place_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
